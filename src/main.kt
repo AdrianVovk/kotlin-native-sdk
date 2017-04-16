58ce463be0255e68be120be4cefa0469e595e12e
Adrian Vovk <adrianvovk@gmail.com>
2017-04-16 13:39:12 -0400

    Add includedFunctions support to make included libries smaller

--- src/main.kt
+++ src/main.kt
@@ -1,5 +1,13 @@
 import sdk.util.log.*
 
+//import gtk.*
+//import gtk.GtkWindowType
+//import gtk.gtk_window_new
+//import gtk.gtk_window_set_title
+//import gtk.gtk_widget_show_all
+//import gtk.G_APPLICATION_FLAGS_NONE
+//import gtk.gtk_application_new
+
 fun main(args: Array<String>) {
 	App()
 }
@@ -8,8 +16,14 @@ class App {
 	init {
 		info("Starting Up", tag = "Overriding Broken Reflection System")
 		debug("Testing Debug")
+		
+		
+		val application = gtk.gtk_application_new("demo.app.Test", 0)
 
-
+		//val window = gtk_window_new(GtkWindowType.GTK_WINDOW_TOPLEVEL)
+		//gtk_window_set_title(window, "AYY")
+		//gtk_widget_show_all(window)
+		
 		//fatal("Crashing Now", code = 9)
 	}	
 }
